9a1443d5e4fa3de7774d3db56f68e2b2dc8239e9
Lucho <lsancheztome@alumnos.exa.unicen.edu.ar>
2026-05-13 14:05:47 -0300

    agrego interfaz  y template game-list

--- src/app/game-list/game-list.ts
+++ src/app/game-list/game-list.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
+import { Game } from './game';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-game-list',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './game-list.html',
   styleUrl: './game-list.scss',
 })
 export class GameList {
   
-  game = {
-    "nombre": "Red Dead Redemption 2",
-    "genero": "Mundo Abierto, Acción, Aventura",
-    "precio": 59.99,
-    "stock": 100,
-    "img": "/assets/img/RDR2.jpeg"
-  }
+  games: Game[] = [
+    {
+    nombre: "Red Dead Redemption 2",
+    genero: "Mundo Abierto, Acción, Aventura",
+    precio: 59.99,
+    stock: 100,
+    img: "/assets/img/RDR2.jpeg",
+    clearance: false,
+  },
+
+  {
+    nombre: "The Last OF Us",
+    genero: "Acción, Aventura",
+    precio: 69.99,
+    stock: 150,
+    img: "/assets/img/TLOU.jpeg",
+    clearance: true,
+  },
+
+  {
+    nombre: "Assassin's Creed",
+    genero: "Mundo Abierto, Acción, Aventura, Sigilo",
+    precio: 40.00,
+    stock: 700,
+    img: "/assets/img/AC.jpeg",
+    clearance: false,
+  },
+];
 }
+ 
---
+++ src/app/game-list/game.ts
@@ -0,0 +1,8 @@
+export interface Game {
+    nombre: string;
+    genero: string;
+    precio: number;
+    stock: number;
+    img: string;
+    clearance: boolean;
+}
